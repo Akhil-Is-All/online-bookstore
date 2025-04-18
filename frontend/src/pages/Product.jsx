9a8b6df33f4c839ad56bc817d9670c910bd9c503
Akhil-Is-All <singhakhil0231@gmail.com>
2025-04-18 12:01:11 +0530

    updated files

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,7 +2,10 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
-import RelatedProducts from '../components/RelatedProducts';
+import RelatedProducts from '../components/Relatedproducts';
+
+
+
 
 const Product = () => {
   const {productId}=useParams();
