ab968deb77006bc244cfe7726ffbab46d670d7d7
Akhil-Is-All <singhakhil0231@gmail.com>
2025-04-20 10:53:39 +0530

    updated files

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -17,7 +17,7 @@ const BestSeller = () => {
   return (
     <div className='my-10'>
         <div className="text-center text-3xl py-8">
-            <Title text1={'BEST'} text2={'SELLER'}/>
+            <Title text1={'BEST'} text2={'SELLERS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
             {/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi exercitationem nisi ipsa? Placeat veritatis assumenda eligendi incidunt, mollitia dolor rem ipsum magni quaerat! */}
             </p>
